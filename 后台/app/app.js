cd1f255fd6383bf9c1d336a4a0cc3234621e481d
Raymond_Win <notmoirae@foxmail.com>
2019-07-08 15:06:22 +0800

    everything fixed! congradulations dude

--- 后台/app/app.js
+++ 后台/app/app.js
@@ -23,7 +23,7 @@ app.all('*', function(req, res, next) {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({extended: true}));
 app.use(logger('dev'));
